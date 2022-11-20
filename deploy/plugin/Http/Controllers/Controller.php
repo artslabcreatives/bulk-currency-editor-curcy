822cf910ccbf60d1dc6e978502473052743e7fb5
voxsar <voxsar@users.noreply.github.com>
2022-11-20 20:03:03 +0000

    Commit from GitHub Actions (MasterDeployCI)

--- deploy/plugin/Http/Controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BulkCurrencyEditorCURCY\Http\Controllers;
-
-use BulkCurrencyEditorCURCY\WPBones\Routing\Controller as BaseController;
-
-abstract class Controller extends BaseController {
-
-}
