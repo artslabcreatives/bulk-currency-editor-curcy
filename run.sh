f0ef1e683179dd2f4e6cac64fffb0ee30d378e16
Miyuru Dharmage <voxsar@gmail.com>
2022-11-20 19:26:47 +0530

    changes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-php bones deploy updates2
+sudo php bones deploy updates2
 ls -l
 zip -r artlab-bulk-product-editor.zip updates2
 zip -r artlab-bulk-product-editor2.zip artlab-bulk-product-editor.zip
