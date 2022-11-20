ce30e44f4cecd412a093144aee01f69fb19814ae
Miyuru Dharmage <voxsar@gmail.com>
2022-11-20 23:45:30 +0530

    changes

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@ sudo apt install nginx
 sudo apt install mysql-server
 sudo apt install php-fpm php-mysql
 
+cd /home/runner/work/bulk-currency-editor-curcy/bulk-currency-editor-curcy/
+sudo cp config.conf /etc/nginx/sites-available/config.conf
+sudo ln -s /etc/nginx/sites-available/config.conf /etc/nginx/sites-enabled/config
+sudo service nginx restart
 
 sudo chown -R $USER ~/.composer && sudo composer install
 zip -r temp.zip .
@@ -13,11 +17,6 @@ mkdir wordpress && cd wordpress && wp core download
 cd .. && ls -l
 mv temp.zip wordpress/wp-content/plugins
 cd wordpress/wp-content/plugins && unzip temp.zip -d temp && cd temp
-
-cd /home/runner/work/bulk-currency-editor-curcy/bulk-currency-editor-curcy/
-sudo cp config.conf /etc/nginx/sites-available/config.conf
-sudo ln -s /etc/nginx/sites-available/config.conf /etc/nginx/sites-enabled/config
-sudo service nginx restart
 
 cd /home/runner/work/bulk-currency-editor-curcy/bulk-currency-editor-curcy/wordpress/wp-content/plugins/temp/
 sudo php bones
